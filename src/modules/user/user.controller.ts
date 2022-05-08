e48a9f829257bae3f0b0bbed40759a6a6f098d78
Matheus Reis <matheus.reis081@gmail.com>
2022-05-08 01:37:15 -0300

    Módulo de usuário - rotas de perfil funcionando

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,16 +1,26 @@
-import { Controller, Get, Param, ParseUUIDPipe } from "@nestjs/common";
+import { Controller, Get, Param, ParseUUIDPipe, Req, Res, UseGuards } from "@nestjs/common";
+import { AdminGuard } from "../auth/admin/admin-auth.guard";
+import { JwtAuthGuard } from "../auth/jwt/jwt-auth.guard";
 import { UserService } from "./user.service";
 
 
-
+@UseGuards(JwtAuthGuard)
 @Controller("/user")
 export class UserController{
 
 constructor(private readonly userService: UserService){}
-    
-@Get(":id")
-listDataUser(@Param("id", new ParseUUIDPipe()) id: string){
-    return this.userService.listDataUser(id)
-}    
+  
+@UseGuards(AdminGuard)
+@Get("/perfil/:id")
+listUserById(@Param("id", new ParseUUIDPipe()) id: string){
+    return this.userService.listUserById(id)
+} 
+
+
+@Get("/perfil")
+listDataUser(@Req() request:any){
+    console.log(request)
+    return this.userService.listDataUser(request)
+}
 
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,7 +3,11 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { UserRepository } from "./user.repository";
 
-
+interface IRequest extends Request {
+  user: {
+    user_id: string
+  }
+}
 
 
 @Injectable()
@@ -14,7 +18,26 @@ export class UserService {
     ){}
 
 
-      listDataUser(id: string){
-        return this.userRepository.findOne({  })
+      async listUserById(id: string){
+        const {name, username, perfil } = await this.userRepository.findById(id)
+
+        return {
+          name,
+          username,
+          perfil
+        }
       }
+
+
+      async listDataUser(request: IRequest){
+
+        const id = request.user.user_id
+        const {name, username, perfil } = await this.userRepository.findById(id)
+
+        return {
+          name,
+          username,
+          perfil
+        }
+      } 
 }
